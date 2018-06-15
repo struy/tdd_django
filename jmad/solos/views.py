8a3dcdb7d51866185d8241b04a28f8475e72d4a0
struy <dubovykwork@gmail.com>
2018-06-15 11:08:53 +0300

    initial index template, view test

--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 # Create your views here.
-def index():
-    pass
+def index(request):
+    # return HttpResponse()
+    return render_to_response('solos/index.html')
+    
