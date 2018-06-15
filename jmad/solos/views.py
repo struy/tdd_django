0cc116a33113f139e6b043937955f050e3341518
struy <dubovykwork@gmail.com>
2018-06-15 11:38:58 +0300

    Bug fix path template and tests module

--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-# Create your views here.
+
 def index(request):
     # return HttpResponse()
     return render_to_response('solos/index.html')
