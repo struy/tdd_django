adac42d3ccb2c570002df7bfc3ecd5cdae710b14
struy <dubovykwork@gmail.com>
2018-06-15 18:37:50 +0300

    Adds Solo model, implemented in index view

--- jmad/solos/tests/test_views.py
+++ jmad/solos/tests/test_views.py
@@ -1,9 +1,23 @@
 from django.test import TestCase, RequestFactory
 from solos.views import index
+from django.db.models.query import QuerySet
+from solos.models import Solo
 
 class IndexViewTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+
+        self.drum_solo = Solo.objects.create(
+            instrument='drums',
+            artist='Rich',
+            track='Bugle Call Rag'
+        )
+        self.bass_solo = Solo.objects.create(
+            instrument='saxophone',
+            artist='Coltrane',
+            track='Mr. PC'
+        )
+
 
     def test_index_view_basic(self):
         """
@@ -14,3 +28,23 @@ class IndexViewTestCase(TestCase):
         with self.assertTemplateUsed('solos/index.html'):
             response = index(request)
             self.assertEqual(response.status_code, 200)
+    def test_index_view_returns_solos(self):
+        """
+        Test that the index view will attempt to return
+        Solos if query parameters exist
+        """
+        
+        response = self.client.get(
+        '/',
+        {'instrument': 'drums'}
+        )
+        solos = response.context['solos']
+        self.assertIs(type(solos), QuerySet)
+        self.assertEqual(len(solos), 1)
+        self.assertEqual(solos[0].artist, 'Rich')
+
+
+
+
+
+      
--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from solos.models import Solo
 
 
 def index(request):
     # return HttpResponse()
-    return render_to_response('solos/index.html')
+    context = {'solos': Solo.objects.filter(
+        instrument=request.GET.get(
+        'instrument', None))}
+    return render_to_response('solos/index.html', context)
     
