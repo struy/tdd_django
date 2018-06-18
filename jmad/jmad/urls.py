5542495f61064b7f662bab123440a3d55658dc4a
struy <dubovykwork@gmail.com>
2018-06-18 10:49:11 +0300

    Adds a tag to search result, small test refactor

--- jmad/jmad/urls.py
+++ jmad/jmad/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from solos.views import SoloDetailView
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'solos.views.index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^solos/(?P<pk>\d+)/$', SoloDetailView.as_view()),
 ]
