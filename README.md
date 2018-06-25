9a97411ff075d24ceadc7b908ef8bb87f6273fac
struykm@yandex.ru <struykm@yandex.ru>
2018-06-25 22:49:01 +0300

    remove js css tests

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Test-Driven Development with Django
 
 chmod +x geckodriver
 sudo cp geckodriver /usr/local/bin/
+
+apt-get install sloccount
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+
--- jmad/jmad/settings.py
+++ jmad/jmad/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS += ('django_jenkins',)
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
+    # 'django_jenkins.tasks.run_jslint',
+    # 'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount'
 )
 
